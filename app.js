11cb10dad7e7b246794ca1331ece2a0bce70b9e1
Maaz-Ahmed <wwwmaazahmed1@gmail.com>
2025-12-25 19:18:52 +0500

    major-update

---
+++ app.js
@@ -0,0 +1,150 @@
+
+// for(let// QUECTION--1
+// let firstname = prompt('enter some name')
+// let lastname = prompt('enter name')
+// let fullname = `${firstname}${lastname}`
+// alert('welcome'+fullname)
+
+// QUECTION--2
+// let search = prompt('enter your mobile model name');
+// let updat_search = search.replaceAll(" ","").length;
+// alert(updat_search)
+
+// quection --3
+// let index = prompt(123);
+// let search = prompt(345);
+// ok in index){
+//     if(index[ok]===search){
+//         grade++
+//     }
+// }
+//         alert(grade)
+
+// QUECTION--4;
+// let hello = 'hello world'
+// console.log(hello.lastIndexOf('l'))
+
+// quection--5;
+// let ch = 'pakistan';
+// const  ok = ch.charAt(3)
+// alert(ok)
+
+// quection--7;
+// let heder = 'hederabad'
+// let ok = heder.replace('heder','islam')
+// console.log(ok)
+
+// // QUECTION--8
+// let message = 'ali and sami are best friend ,they played cricket and footbal to geather'
+// console.log(message.replaceAll('and','&'))
+
+// QUECTION--9
+// let str = '234'
+// let num = Number(str)
+
+// console.log(typeof num)
+
+// QUECTION--10
+// let ok = prompt('enter some lower case number');
+// let answer = ok.toUpperCase()
+// alert(`your answer is ${answer }`)
+
+// // QUECTION--11;
+// let user_input = 'ok';
+// let geo = user_input.toLowerCase().split(' ')
+// for(let i = 0; i < geo.length; i++ ){
+//     geo[i]= geo[i][0].toUpperCase()+geo[i].slice(1)
+// }
+// console.log(geo)
+
+// QUECTION--12
+// let num = 23.13;
+// let ok = num.toString() 
+// let result=ok.replaceAll('.','')
+// console.log(result)
+// quection--13;
+// let user = 33;
+// let ok = user;
+// if(ok===33||ok===44||ok===46||ok===64){
+//     console.log('ok')
+// }
+
+// QUECTION--14;
+// let A = ['cake','mango','banana','grey']
+// let prom = 'cak';
+// if(A.includes(prom)){
+//     console.log('ok')
+// }else{
+//     console.log('error')
+// }
+
+// QUECTION--15;
+// function invalidnumber (password){
+//    if(password.length<6){
+//     return false
+//    }
+//    let firstchar = password.charAt(0);
+//    if(firstchar>1&&firstchar<9){
+//     return false
+//    }
+//    let hasalphabet = false;
+//    let hasNumber = false;
+
+//    for (let i = 0; i < password.length; i++) {
+//         let ch = password.charAt(i);
+//         if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+//             hasAlphabet = true;
+//         } else if (ch >= '0' && ch <= '9') {
+//             hasNumber = true;
+//         }
+//     return hasalphabet && hasNumber; 
+//     }
+// }
+//     let userpassword = prompt();
+//     while(!invalidnumber(userpassword)){
+//          userpassword = prompt("Invalid password! Please enter a valid password:");
+//     }
+// alert('password accepted')
+    // quection--16
+// let university = 'university of karachi';
+// console.log(university.split(''))
+
+// let found = false;
+// for(let i = 0;i<userInput.toLowerCase();i++){
+//     if(A[i].indexOf()===ok){
+//         // console.log('ok')
+//         found= true;
+//         break;
+//     }
+// }
+
+// if(found){
+//     console.log('worling')
+// }else{
+//     console.log('not working')
+// }
+
+// console.log(ok)
+// if(ok===search){
+//     console.log('done')
+    // for(let i = A;i<=A;i++){
+    // search===A[i]
+    // search.includes(i)
+    // console.log(search)
+// }
+// }
+// QUECTION--17
+// let word = 'ok';
+// console.log(word[word.length-1]);
+
+// QUECTION--18;
+let user = 'The quick brown fox jumps over the lazy dog';
+user = user.toLowerCase();
+let gum = user.split(' ')
+let Ge = 0;
+for(let i = 0;i < gum.length;i++){
+    if(gum[i]==='the'){
+   Ge++;
+    }
+}
+console.log(Ge)
